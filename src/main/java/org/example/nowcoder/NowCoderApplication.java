9252558ebced77443ef27f1759714fb66496c3f7
zhaoShuai <s74933347@gmail.com>
2025-12-30 22:24:48 +0800

    feature: Add project basic architecture configuration

---
+++ src/main/java/org/example/nowcoder/NowCoderApplication.java
@@ -0,0 +1,16 @@
+package org.example.nowcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 23211
+ */
+@SpringBootApplication
+public class NowCoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowCoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/nowcoder/NowCoderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nowcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowCoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
